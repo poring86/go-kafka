9f29c0a68e92ed52f82403d0f09d4e8d1faa6600
poring86 <matheuslino86a@gmail.com>
2025-01-02 15:25:09 -0400

    publish message

---
+++ cmd/producer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	producer := NewKafkaProducer()
+	Publish("Mensagem", "teste", producer, nil)
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.server": "go-kafka-kafka-1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-kafka
+
+go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
